f490936ee002606df5bf896e7e9897aeb77b914c
SebastianJaimes <sebastianjaimesg@gmail.com>
2025-09-02 21:36:39 -0500

    Create Data.sql

---
+++ Data.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE olist_db;
+USE olist_db;
+
+--Create tables of the database
+-- Customers
+CREATE TABLE customers (
+    customer_id VARCHAR(50) PRIMARY KEY,
+    customer_unique_id VARCHAR(50),
+    customer_zip_code_prefix INT,
+    customer_city VARCHAR(50),
+    customer_state VARCHAR(2)
+);
+
+-- Orders
+CREATE TABLE orders (
+    order_id VARCHAR(50) PRIMARY KEY,
+    customer_id VARCHAR(50),
+    order_status VARCHAR(20),
+    order_purchase_timestamp DATETIME,
+    order_approved_at DATETIME,
+    order_delivered_carrier_date DATETIME,
+    order_delivered_customer_date DATETIME,
+    order_estimated_delivery_date DATETIME,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+-- Products
+CREATE TABLE products (
+    product_id VARCHAR(50) PRIMARY KEY,
+    product_category_name VARCHAR(100),
+    product_name_lenght INT,
+    product_description_lenght INT,
+    product_photos_qty INT,
+    product_weight_g INT,
+    product_length_cm INT,
+    product_height_cm INT,
+    product_width_cm INT
+);
