b3e6805b7f41b1d2b1d1f77da9f4fabbdf12b556
SebastianJaimes <sebastianjaimesg@gmail.com>
2025-09-10 05:50:02 -0500

    Update Data.sql

--- Data.sql
+++ Data.sql
@@ -124,3 +124,14 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
+LOAD DATA INFILE '/path/to/olist_orders_dataset.csv'
+INTO TABLE orders
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(order_id, customer_id, order_status, order_purchase_timestamp, 
+ order_approved_at, order_delivered_carrier_date, 
+ order_delivered_customer_date, order_estimated_delivery_date);
+
+
