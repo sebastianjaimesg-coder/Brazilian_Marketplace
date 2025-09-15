9e306d04f33b380b42dc09959a66b3a30568d9fc
SebastianJaimes <sebastianjaimesg@gmail.com>
2025-09-14 19:47:04 -0500

    Update Data.sql

--- Data.sql
+++ Data.sql
@@ -157,3 +157,15 @@ IGNORE 1 ROWS
  order_delivered_customer_date, order_estimated_delivery_date);
 
 
+-- Orders
+CREATE TABLE orders (
+    order_id VARCHAR(50) PRIMARY KEY,
+    customer_id VARCHAR(50),
+    order_status VARCHAR(20),
+    order_purchase_timestamp DATETIME,
+    order_approved_at DATETIME,
+    order_delivered_carrier_date DATETIME,
+    order_delivered_customer_date DATETIME,
+    order_estimated_delivery_date DATETIME,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+
