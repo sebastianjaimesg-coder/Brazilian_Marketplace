356b7724d869f5d831bba1311df89371771ffad8
SebastianJaimes <sebastianjaimesg@gmail.com>
2025-09-22 20:57:42 -0500

    Update Data.sql

--- Data.sql
+++ Data.sql
@@ -118,3 +118,30 @@ for chunk in pd.read_csv("olist_customers_dataset.csv", chunksize=50000):
     product_height_cm INT,
     product_width_cm INT
 );
+
+
+-- Orders
+CREATE TABLE orders (
+    order_id VARCHAR(50) PRIMARY KEY,
+    customer_id VARCHAR(50),
+    order_status VARCHAR(20),
+    order_purchase_timestamp DATETIME,
+    order_approved_at DATETIME,
+    order_delivered_carrier_date DATETIME,
+    order_delivered_customer_date DATETIME,
+    order_estimated_delivery_date DATETIME,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+-- Products
+CREATE TABLE products (
+    product_id VARCHAR(50) PRIMARY KEY,
+    product_category_name VARCHAR(100),
+    product_name_lenght INT,
+    product_description_lenght INT,
+    product_photos_qty INT,
+    product_weight_g INT,
+    product_length_cm INT,
+    product_height_cm INT,
+    product_width_cm INT
+);
