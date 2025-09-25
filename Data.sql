8160ac4482b0cf515d0f94848ea3d1058f589b55
SebastianJaimes <sebastianjaimesg@gmail.com>
2025-09-25 16:24:08 -0500

    Update Data.sql

--- Data.sql
+++ Data.sql
@@ -198,3 +198,17 @@ CREATE TABLE products (
     product_height_cm INT,
     product_width_cm INT
 );
+
+
+-- Orders
+CREATE TABLE orders (
+    order_id VARCHAR(50) PRIMARY KEY,
+    customer_id VARCHAR(50),
+    order_status VARCHAR(20),
+    order_purchase_timestamp DATETIME,
+    order_approved_at DATETIME,
+    order_delivered_carrier_date DATETIME,
+    order_delivered_customer_date DATETIME,
+    order_estimated_delivery_date DATETIME,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
